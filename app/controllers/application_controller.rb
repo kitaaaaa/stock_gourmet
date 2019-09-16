0e4bcab9d27226ac137302619eb8cc9217f14c84
kitaaaaa <ryo.bawdies@gmail.com>
2019-09-16 19:09:17 +0900

    add google_map

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	def map
+		results = Geocoder.search(params[:address])
+		@latlng = results.first.coordinates
+		# これでmap.js.erbで、経度緯度情報が入った@latlngを使える
+  		# respond_to以下の記述によって、
+  		# remote: trueのアクセスに対して、
+	 	# map.js.erbが変えるようになります。
+  		respond_to do |format|
+    		format.js
+		end
+	end
+
 	protected
 	def configure_permitted_parameters
     	devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
