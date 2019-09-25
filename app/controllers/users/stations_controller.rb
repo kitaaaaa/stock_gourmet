73ab96bf0255629d37f46582da843bb319611630
kitaaaaa <ryo.bawdies@gmail.com>
2019-09-25 19:25:58 +0900

    fix users_station

--- app/controllers/users/stations_controller.rb
+++ app/controllers/users/stations_controller.rb
@@ -2,7 +2,6 @@ class Users::StationsController < ApplicationController
 	def show
 		@user = current_user
 		@station = Station.find(params[:id])
-		@station_trains = @station.station_trains
 		@restaurant = @station.restaurants
 		@q = Restaurant.ransack(params[:q])
   	@restaurants = @q.result(distinct: true)
