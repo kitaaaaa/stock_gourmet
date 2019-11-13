8df4b9f5ee1fb481aa7a02ef9d69557b704aa908
kitaaaaa <ryo.bawdies@gmail.com>
2019-11-13 18:13:18 +0900

    finish release

--- db/migrate/20190910093728_create_relationships.rb
+++ db/migrate/20190910093728_create_relationships.rb
@@ -1,6 +1,6 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
-    create_table :relationships do |t|
+    create_table :relationships, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
     	t.references :user, foreign_key: true
     	t.references :follow, foreign_key: { to_table: :users }
       t.timestamps
